d5d102131626a7b422139b621aba609a5257e01b
Leif Zuttermeister <217350735+leif-zuttermeister@users.noreply.github.com>
2025-06-23 16:14:24 +0200

    Add color scheme and text theme use cases in example

--- example/example_werkbank/lib/src/example_werkbank/use_cases/sections.dart
+++ example/example_werkbank/lib/src/example_werkbank/use_cases/sections.dart
@@ -1,10 +1,12 @@
 import 'package:example_werkbank/src/example_werkbank/use_cases/components/components.dart';
 import 'package:example_werkbank/src/example_werkbank/use_cases/pages/pages.dart';
+import 'package:example_werkbank/src/example_werkbank/use_cases/theme_use_cases.dart';
 import 'package:werkbank/werkbank.dart';
 
 WerkbankSections get sections => WerkbankSections(
   children: [
     componentsFolder,
     pagesFolder,
+    themeFolder,
   ],
 );
---
+++ example/example_werkbank/lib/src/example_werkbank/use_cases/theme_use_cases.dart
@@ -0,0 +1,55 @@
+import 'package:example_werkbank/src/example_werkbank/use_case_index.dart';
+
+WerkbankFolder get themeFolder => WerkbankFolder(
+  name: 'Theme',
+  children: [
+    WerkbankUseCase(
+      name: 'Color Scheme',
+      builder: colorsUseCaseBuilder(
+        builder: (c) {
+          c.overview
+            ..minimumSize(width: 500, height: 500)
+            ..withoutPadding();
+        },
+        colors: (context) {
+          final colorScheme = Theme.of(context).colorScheme;
+          return {
+            'Primary': colorScheme.primary,
+            'On Primary': colorScheme.onPrimary,
+            'Primary Container': colorScheme.primaryContainer,
+            'On Primary Container': colorScheme.onPrimaryContainer,
+            'Surface': colorScheme.surface,
+            'On Surface': colorScheme.onSurface,
+          };
+        },
+      ),
+    ),
+    WerkbankUseCase(
+      name: 'Text Theme',
+      builder: textStylesUseCaseBuilder(
+        builder: (c) {
+          c.overview
+            ..minimumSize(width: 800, height: 800)
+            ..withoutPadding();
+        },
+        styles: (BuildContext context) {
+          final textTheme = Theme.of(context).textTheme;
+          return {
+            'Display Large': textTheme.displayLarge!,
+            'Display Medium': textTheme.displayMedium!,
+            'Display Small': textTheme.displaySmall!,
+            'Headline Large': textTheme.headlineLarge!,
+            'Headline Medium': textTheme.headlineMedium!,
+            'Headline Small': textTheme.headlineSmall!,
+            'Title Large': textTheme.titleLarge!,
+            'Title Medium': textTheme.titleMedium!,
+            'Title Small': textTheme.titleSmall!,
+            'Body Large': textTheme.bodyLarge!,
+            'Body Medium': textTheme.bodyMedium!,
+            'Body Small': textTheme.bodySmall!,
+          };
+        },
+      ),
+    ),
+  ],
+);

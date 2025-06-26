710f9ab2da8869c38f83b9bdb4f621fd3b426953
Lenard Wiedekamp <lenwdk@gmail.com>
2025-06-26 14:43:33 +0200

    moved files according to subpackage-structure

---
+++ packages/werkbank/lib/src/_internal/src/filter/filter.dart
@@ -0,0 +1,10 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+/// Generated using index_generator package.
+library filter;
+
+export 'src/debug_display_filter.dart';
+export 'src/filter_result.dart';
+export 'src/root_descriptor_filter.dart';
+export 'src/search_cluster_result.dart';
+export 'src/search_result.dart';

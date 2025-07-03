d910404c6e9ed38b3902170fa5dfb60bef2f9ba6
Lenard Wiedekamp <lenwdk@gmail.com>
2025-07-04 00:13:02 +0200

    Add description and tag

--- apps/werkbank_werkbank/lib/component_use_cases/w_preview_height_use_case.dart
+++ apps/werkbank_werkbank/lib/component_use_cases/w_preview_height_use_case.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:werkbank/werkbank.dart';
+import 'package:werkbank_werkbank/tags.dart';
 
 const _loremIpsum = '''
 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
 ''';
 
 WidgetBuilder wCollapsableHeightUseCase(UseCaseComposer c) {
+  c
+    ..description(
+      'By using a `WCollapsableHeight`, you can '
+      'create a widget that can be expanded to its '
+      'intrinsic height or collapsed to a fixed height. ',
+    )
+    ..tags([Tags.feature]);
+
   final collapsedHeight = c.knobs.doubleSlider(
     'collapsedHeight',
     initialValue: 200,

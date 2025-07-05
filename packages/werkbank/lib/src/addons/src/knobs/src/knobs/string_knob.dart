ec19ea537b70f2842c9abf7b2f285d2d1b0fbadd
Leif Zuttermeister <217350735+leif-zuttermeister@users.noreply.github.com>
2025-07-05 21:31:14 +0200

    Migrate string knob to use input knob

--- packages/werkbank/lib/src/addons/src/knobs/src/knobs/string_knob.dart
+++ packages/werkbank/lib/src/addons/src/knobs/src/knobs/string_knob.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:werkbank/werkbank.dart';
 
 /// {@category Knobs}
@@ -7,15 +6,11 @@ extension StringKnobExtension on KnobsComposer {
     String label, {
     required String initialValue,
   }) {
-    return makeRegularKnob(
+    return input<String>(
       label,
       initialValue: initialValue,
-      knobBuilder: (context, valueNotifier) => _StringKnob(
-        valueNotifier: valueNotifier,
-        isMultiLine: false,
-        enabled: true,
-      ),
-      rebuildKnobBuilderOnChange: false,
+      parser: InputParseSuccess.new,
+      formatter: _identityFormatter,
     );
   }
 
@@ -23,16 +18,11 @@ extension StringKnobExtension on KnobsComposer {
     String label, {
     required String initialValue,
   }) {
-    return makeRegularKnob(
+    return inputMultiLine<String>(
       label,
       initialValue: initialValue,
-      knobBuilder: (context, valueNotifier) => _StringKnob(
-        valueNotifier: valueNotifier,
-        isMultiLine: true,
-        enabled: true,
-      ),
-      rebuildKnobBuilderOnChange: false,
-      forceSpaciousLayout: true,
+      parser: InputParseSuccess.new,
+      formatter: _identityFormatter,
     );
   }
 }
@@ -43,16 +33,12 @@ extension NullableStringKnobExtension on NullableKnobs {
     required String initialValue,
     bool initiallyNull = false,
   }) {
-    return makeNullableKnob(
+    return input<String>(
       label,
       initialValue: initialValue,
+      parser: InputParseSuccess.new,
+      formatter: _identityFormatter,
       initiallyNull: initiallyNull,
-      knobBuilder: (context, enabled, valueNotifier) => _StringKnob(
-        valueNotifier: valueNotifier,
-        isMultiLine: false,
-        enabled: enabled,
-      ),
-      rebuildKnobBuilderOnChange: false,
     );
   }
 
@@ -61,72 +47,14 @@ extension NullableStringKnobExtension on NullableKnobs {
     required String initialValue,
     bool initiallyNull = false,
   }) {
-    return makeNullableKnob(
+    return inputMultiLine<String>(
       label,
       initialValue: initialValue,
+      parser: InputParseSuccess.new,
+      formatter: _identityFormatter,
       initiallyNull: initiallyNull,
-      knobBuilder: (context, enabled, valueNotifier) => _StringKnob(
-        valueNotifier: valueNotifier,
-        isMultiLine: true,
-        enabled: enabled,
-      ),
-      rebuildKnobBuilderOnChange: false,
-      forceSpaciousLayout: true,
     );
   }
 }
 
-class _StringKnob extends StatefulWidget {
-  const _StringKnob({
-    required this.valueNotifier,
-    required this.isMultiLine,
-    required this.enabled,
-  });
-
-  final ValueNotifier<String> valueNotifier;
-  final bool isMultiLine;
-  final bool enabled;
-
-  @override
-  State<_StringKnob> createState() => _StringKnobState();
-}
-
-class _StringKnobState extends State<_StringKnob> {
-  late TextEditingController _textEditingController;
-
-  @override
-  void initState() {
-    super.initState();
-    _textEditingController = TextEditingController(
-      text: widget.valueNotifier.value,
-    );
-    _textEditingController.addListener(_textEditingControllerChanged);
-    widget.valueNotifier.addListener(_knobChanged);
-  }
-
-  void _textEditingControllerChanged() {
-    widget.valueNotifier.value = _textEditingController.text;
-  }
-
-  void _knobChanged() {
-    if (widget.valueNotifier.value != _textEditingController.text) {
-      _textEditingController.text = widget.valueNotifier.value;
-    }
-  }
-
-  @override
-  void dispose() {
-    widget.valueNotifier.removeListener(_knobChanged);
-    _textEditingController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return WTextField(
-      controller: _textEditingController,
-      maxLines: widget.isMultiLine ? 3 : 1,
-      enabled: widget.enabled,
-    );
-  }
-}
+String _identityFormatter(String value) => value;

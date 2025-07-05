bc92be78f2c8d266301df6133b1f570869c03fe8
Leif Zuttermeister <217350735+leif-zuttermeister@users.noreply.github.com>
2025-07-05 21:49:09 +0200

    Migrate int knob to use customSlider knob

--- packages/werkbank/lib/src/addons/src/knobs/src/knobs/int_knob.dart
+++ packages/werkbank/lib/src/addons/src/knobs/src/knobs/int_knob.dart
@@ -1,6 +1,3 @@
-import 'dart:core';
-
-import 'package:flutter/material.dart';
 import 'package:werkbank/werkbank.dart';
 
 /// {@category Knobs}
@@ -10,18 +7,17 @@ extension IntKnobExtension on KnobsComposer {
     required int initialValue,
     int min = 0,
     int max = 10,
-    IntFormatter valueFormatter = _IntSliderKnob.defaultFormatter,
+    IntFormatter valueFormatter = _defaultFormatter,
   }) {
-    return makeRegularKnob(
+    return customSlider(
       label,
       initialValue: initialValue,
-      knobBuilder: (context, valueNotifier) => _IntSliderKnob(
-        valueNotifier: valueNotifier,
-        min: min,
-        max: max,
-        valueFormatter: valueFormatter,
-        enabled: true,
-      ),
+      min: min,
+      max: max,
+      divisions: max - min,
+      doubleEncoder: (v) => v.toDouble(),
+      doubleDecoder: (d) => d.toInt(),
+      valueFormatter: valueFormatter,
     );
   }
 }
@@ -33,57 +29,22 @@ extension NullableIntKnobExtension on NullableKnobs {
     bool initiallyNull = false,
     int min = 0,
     int max = 10,
-    IntFormatter valueFormatter = _IntSliderKnob.defaultFormatter,
+    IntFormatter valueFormatter = _defaultFormatter,
   }) {
-    return makeNullableKnob(
+    return customSlider(
       label,
       initialValue: initialValue,
       initiallyNull: initiallyNull,
-      knobBuilder: (context, enabled, valueNotifier) => _IntSliderKnob(
-        valueNotifier: valueNotifier,
-        min: min,
-        max: max,
-        valueFormatter: valueFormatter,
-        enabled: enabled,
-      ),
+      min: min,
+      max: max,
+      divisions: max - min,
+      doubleEncoder: (v) => v.toDouble(),
+      doubleDecoder: (d) => d.toInt(),
+      valueFormatter: valueFormatter,
     );
   }
 }
 
 typedef IntFormatter = String Function(int value);
 
-class _IntSliderKnob extends StatelessWidget {
-  const _IntSliderKnob({
-    required this.valueNotifier,
-    required this.min,
-    required this.max,
-    required this.valueFormatter,
-    required this.enabled,
-  });
-
-  static String defaultFormatter(int value) {
-    return value.toString();
-  }
-
-  final ValueNotifier<int> valueNotifier;
-  final int min;
-  final int max;
-  final IntFormatter valueFormatter;
-  final bool enabled;
-
-  @override
-  Widget build(BuildContext context) {
-    return WSlider(
-      value: valueNotifier.value.toDouble(),
-      onChanged: enabled
-          ? (value) {
-              valueNotifier.setValue(value.toInt());
-            }
-          : null,
-      min: min.toDouble(),
-      max: max.toDouble(),
-      divisions: max - min,
-      valueFormatter: (value) => valueFormatter(value.toInt()),
-    );
-  }
-}
+String _defaultFormatter(int value) => value.toString();

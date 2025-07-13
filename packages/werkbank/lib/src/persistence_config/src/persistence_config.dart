7403d42d34f59058f279179771792d79ac0a9d88
Leif Zuttermeister <217350735+leif-zuttermeister@users.noreply.github.com>
2025-07-13 21:04:09 +0200

    Make PersistenceConfig constructors const

--- packages/werkbank/lib/src/persistence_config/src/persistence_config.dart
+++ packages/werkbank/lib/src/persistence_config/src/persistence_config.dart
@@ -1,26 +1,28 @@
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:werkbank/src/persistence_config/src/store.dart';
 
-class PersistenceConfig {
-  factory PersistenceConfig() {
-    return PersistenceConfig._(
-      createJsonStore: () async => SharedPreferencesStore(
-        await SharedPreferencesWithCache.create(
-          cacheOptions: const SharedPreferencesWithCacheOptions(),
-        ),
+interface class PersistenceConfig {
+  const PersistenceConfig()
+    : this._(createJsonStore: _createSharedPreferencesStore);
+
+  const PersistenceConfig.memory()
+    : this._(createJsonStore: _createMemoryStore);
+
+  const PersistenceConfig._({
+    required Future<JsonStore> Function() createJsonStore,
+  }) : _createJsonStore = createJsonStore;
+
+  static Future<JsonStore> _createSharedPreferencesStore() async {
+    return SharedPreferencesStore(
+      await SharedPreferencesWithCache.create(
+        cacheOptions: const SharedPreferencesWithCacheOptions(),
       ),
     );
   }
 
-  factory PersistenceConfig.memory() {
-    return PersistenceConfig._(
-      createJsonStore: () async => MemoryStore(),
-    );
+  static Future<JsonStore> _createMemoryStore() async {
+    return MemoryStore();
   }
-
-  PersistenceConfig._({
-    required Future<JsonStore> Function() createJsonStore,
-  }) : _createJsonStore = createJsonStore;
 
   final Future<JsonStore> Function() _createJsonStore;
 

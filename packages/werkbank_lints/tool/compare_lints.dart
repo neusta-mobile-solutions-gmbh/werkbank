884b3545e6808514d8558d38888e87568961a0ac
Leif Zuttermeister <217350735+leif-zuttermeister@users.noreply.github.com>
2025-07-15 22:39:06 +0200

    Add tools to generate lints and generate them

---
+++ packages/werkbank_lints/tool/compare_lints.dart
@@ -0,0 +1,9 @@
+import 'lints.dart';
+
+Future<void> main() async {
+  final goalLints = allLints.difference(blacklistedLints);
+  print('The flutter_lints package is missing:');
+  printLintSet(goalLints.difference(lintsFlutterPackageLints));
+  print('\nThe flutter_lints package has extra lints:');
+  printLintSet(lintsFlutterPackageLints.difference(goalLints));
+}
---
+++ packages/werkbank_lints/tool/lints.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:yaml/yaml.dart';
+
+final allLints = getLinterRulesFromPath('tool/lints/all.yaml');
+
+final blacklistedLints = getLinterRulesFromPath('tool/lints/blacklisted.yaml');
+
+final lintsFlutterPackageLints =
+    getLinterRulesFromPath('tool/lints/lints_flutter/flutter.yaml')
+        .union(getLinterRulesFromPath('tool/lints/lints/recommended.yaml'))
+        .union(getLinterRulesFromPath('tool/lints/lints/core.yaml'));
+
+final werkbankLintsPackageLints = getLinterRulesFromPath(
+  'lib/werkbank_lints.yaml',
+);
+
+Set<String> getLinterRulesFromPath(String path) {
+  return getLinterRules(File(path).readAsStringSync());
+}
+
+Set<String> getLinterRules(String yaml) {
+  return (loadYaml(yaml)['linter']['rules'] as YamlList)
+      .map((dynamic e) => e as String)
+      .toSet();
+}
+
+void printLintSet(Set<String> lints) {
+  for (final l in lints.toList()..sort()) {
+    print('- $l');
+  }
+}

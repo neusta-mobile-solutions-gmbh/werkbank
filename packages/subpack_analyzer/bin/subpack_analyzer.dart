c4724e7f43ac95e1f404a78628a2e0303865136d
Leif Zuttermeister <217350735+leif-zuttermeister@users.noreply.github.com>
2025-07-24 20:29:05 +0200

    Fix main

--- packages/subpack_analyzer/bin/subpack_analyzer.dart
+++ packages/subpack_analyzer/bin/subpack_analyzer.dart
@@ -1,17 +1,20 @@
+import 'dart:io';
+
 import 'package:args/command_runner.dart';
 import 'package:subpack_analyzer/src/commands/graph/graph.dart';
 import 'package:subpack_analyzer/src/commands/run/run_command.dart';
 
 Future<void> main(List<String> args) async {
-  Future<void> runCommand() async {
-    final runner = CommandRunner<void>(
-      'analyzer',
-      "A super duper nice tool to analyzer your package's subpackages.",
-    );
-    runner.addCommand(RunCommand());
-    runner.addCommand(GraphCommand());
+  final runner = CommandRunner<void>(
+    'subpack_analyzer',
+    "A tool to analyzer your dart package's subpackages.",
+  );
+  runner.addCommand(RunCommand());
+  runner.addCommand(GraphCommand());
+  try {
     await runner.run(args);
+  } on UsageException catch (e) {
+    stdout.writeln(e.message);
+    exit(64);
   }
-
-  await runCommand();
 }

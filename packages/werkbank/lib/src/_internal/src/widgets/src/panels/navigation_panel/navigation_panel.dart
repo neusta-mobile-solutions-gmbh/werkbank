7b7f4a6928608ef672fe9eb8bed6e49b7f24bdf1
Leif Zuttermeister <217350735+leif-zuttermeister@users.noreply.github.com>
2025-08-22 10:01:03 +0200

    Fix some lints and compile errors

--- packages/werkbank/lib/src/_internal/src/widgets/src/panels/navigation_panel/navigation_panel.dart
+++ packages/werkbank/lib/src/_internal/src/widgets/src/panels/navigation_panel/navigation_panel.dart
@@ -4,7 +4,6 @@ import 'package:werkbank/src/_internal/src/localizations/localizations.dart';
 import 'package:werkbank/src/_internal/src/routing/routing.dart';
 import 'package:werkbank/src/_internal/src/widgets/widgets.dart';
 import 'package:werkbank/src/components/components.dart';
-import 'package:werkbank/src/persistence/persistence.dart';
 import 'package:werkbank/src/routing/routing.dart';
 import 'package:werkbank/src/theme/theme.dart';
 import 'package:werkbank/src/tree/tree.dart';

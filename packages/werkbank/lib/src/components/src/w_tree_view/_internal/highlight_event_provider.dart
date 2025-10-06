0d7268be98cde413a74cd39dbca45709ca941757
Leif Zuttermeister <217350735+leif-zuttermeister@users.noreply.github.com>
2025-10-06 17:43:16 +0200

    Simplify HighlightEventProvider

--- packages/werkbank/lib/src/components/src/w_tree_view/_internal/highlight_event_provider.dart
+++ packages/werkbank/lib/src/components/src/w_tree_view/_internal/highlight_event_provider.dart
@@ -1,44 +1,27 @@
 import 'package:flutter/material.dart';
 
 /// {@category Werkbank Components}
-class HighlightEventProvider extends StatelessWidget {
+class HighlightEventProvider extends InheritedWidget {
   HighlightEventProvider({
     super.key,
-    this.highlightStream,
-    required this.child,
+    required this.highlightStream,
+    required super.child,
   }) : assert(
          highlightStream?.isBroadcast ?? true,
          'highlightStream must be a broadcast stream',
        );
 
   static Stream<List<LocalKey>>? maybeOf(BuildContext context) {
-    final inherited = context.dependOnInheritedWidgetOfExactType<_Inherited>();
+    final inherited = context
+        .dependOnInheritedWidgetOfExactType<HighlightEventProvider>();
     assert(inherited != null, 'No HighlightEventProvider found in context');
     return inherited!.highlightStream;
   }
 
   final Stream<List<LocalKey>>? highlightStream;
-  final Widget child;
 
   @override
-  Widget build(BuildContext context) {
-    return _Inherited(
-      highlightStream: highlightStream,
-      child: child,
-    );
-  }
-}
-
-class _Inherited extends InheritedWidget {
-  const _Inherited({
-    required this.highlightStream,
-    required super.child,
-  });
-
-  final Stream<List<LocalKey>>? highlightStream;
-
-  @override
-  bool updateShouldNotify(covariant _Inherited oldWidget) {
+  bool updateShouldNotify(HighlightEventProvider oldWidget) {
     return highlightStream != oldWidget.highlightStream;
   }
 }

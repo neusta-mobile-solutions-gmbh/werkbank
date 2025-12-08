a38abf6e41ed7b64716d5716134ff768e91d705d
Leif Zuttermeister <217350735+leif-zuttermeister@users.noreply.github.com>
2025-12-08 13:06:38 +0100

    Fix import

--- packages/werkbank/lib/src/addons/src/accessibility/src/_internal/semantics_inspector/semantics_inspector_overlay.dart
+++ packages/werkbank/lib/src/addons/src/accessibility/src/_internal/semantics_inspector/semantics_inspector_overlay.dart
@@ -7,7 +7,7 @@ import 'package:werkbank/src/addons/src/accessibility/src/_internal/semantics_in
 import 'package:werkbank/src/addons/src/accessibility/src/_internal/semantics_inspector/semantics_inspector_controller.dart';
 import 'package:werkbank/src/addons/src/accessibility/src/_internal/semantics_monitor.dart';
 import 'package:werkbank/src/addons/src/accessibility/src/_internal/semantics_nodes_display.dart';
-import 'package:werkbank/src/components/src/w_divider.dart';
+import 'package:werkbank/src/components/components.dart';
 import 'package:werkbank/src/theme/theme.dart';
 
 class SemanticsInspectorOverlay extends StatefulWidget {

d822bdbe1ed285f9f1a44645dc62cd5509ee7b47
Leif Zuttermeister <217350735+leif-zuttermeister@users.noreply.github.com>
2025-12-21 13:06:32 +0100

    Change GlobalStateController

--- packages/werkbank/lib/src/global_state/src/global_state_controller.dart
+++ packages/werkbank/lib/src/global_state/src/global_state_controller.dart
@@ -6,32 +6,41 @@ import 'package:flutter/material.dart';
 /// A controller that manages and persists state that is global to
 /// the whole application.
 ///
-/// When extending this class, you must implement the [tryLoadFromJson]
-/// and [toJson] methods to load and persist the state.
-/// Once the [GlobalStateController] changes its state in a way that
-/// would change the value returned by [toJson], it must call
-/// [notifyListeners] with `hasJsonChanged` set to `true` (the default).
-///
-///
 /// [Addon]s can create and register [GlobalStateController]s by overriding the
 /// [Addon.registerGlobalStateControllers] method.
-abstract class GlobalStateController extends ChangeNotifier {
-  final _jsonChangedNotifier = _JsonChangedNotifier();
+abstract class GlobalStateController {
+  void dispose();
+}
 
+abstract class ValueNotifierGlobalStateController<T> extends ValueNotifier<T>
+    implements GlobalStateController {
+  ValueNotifierGlobalStateController(super._value);
+}
+
+/// When using this mixin, you must implement the [tryLoadFromJson]
+/// and [toJson] methods to load and persist the state.
+/// Once the [GlobalStateController] changes its state in a way that
+/// would change the value returned by [toJson], the [jsonChangedListenable]
+/// must notify its listeners.
+mixin PersistedGlobalStateControllerMixin on GlobalStateController {
   /// A listenable that notifies its listeners if the value returned by
   /// [toJson] has changed.
-  Listenable get jsonChangedListenable => _jsonChangedNotifier;
+  Listenable get jsonChangedListenable;
 
   void tryLoadFromJson(Object? json, {required bool isWarmStart});
 
   Object? toJson();
+}
+
+mixin NotifiablePersistedGlobalStateControllerMixin on GlobalStateController
+    implements PersistedGlobalStateControllerMixin {
+  final _jsonChangedNotifier = _JsonChangedNotifier();
 
   @override
-  void notifyListeners({bool hasJsonChanged = true}) {
-    super.notifyListeners();
-    if (hasJsonChanged) {
-      _jsonChangedNotifier.notifyPersistenceChanged();
-    }
+  Listenable get jsonChangedListenable => _jsonChangedNotifier;
+
+  void notifyJsonChanged() {
+    _jsonChangedNotifier.notifyPersistenceChanged();
   }
 }
 
@@ -39,4 +48,11 @@ class _JsonChangedNotifier extends ChangeNotifier {
   void notifyPersistenceChanged() {
     notifyListeners();
   }
+}
+
+mixin ListeningPersistedGlobalStateControllerMixin
+    on GlobalStateController, Listenable
+    implements PersistedGlobalStateControllerMixin {
+  @override
+  Listenable get jsonChangedListenable => this;
 }
